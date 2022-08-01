00db693f9842a585142ab6ff799f6ee56a9b3a54
kiborgok <borgokalek14@gmail.com>
2022-08-01 15:27:23 +0300

    design cart items

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -3,39 +3,12 @@ import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { XIcon } from "@heroicons/react/outline";
 
-const products = [
-  {
-    id: 1,
-    name: "Throwback Hip Bag",
-    href: "#",
-    color: "Salmon",
-    price: "$90.00",
-    quantity: 1,
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
-    imageAlt:
-      "Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.",
-  },
-  {
-    id: 2,
-    name: "Medium Stuff Satchel",
-    href: "#",
-    color: "Blue",
-    price: "$32.00",
-    quantity: 1,
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
-    imageAlt:
-      "Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.",
-  },
-  // More products...
-];
 
-export default function ShoppingCart({ openCart, setOpenCart }) {
 
+export default function ShoppingCart({ cartItems, cart, onCartToggle, totalPrice }) {
   return (
-    <Transition.Root show={openCart} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={setOpenCart}>
+    <Transition.Root show={cart} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={onCartToggle}>
         <Transition.Child
           as={Fragment}
           enter="ease-in-out duration-500"
@@ -72,7 +45,7 @@ export default function ShoppingCart({ openCart, setOpenCart }) {
                           <button
                             type="button"
                             className="-m-2 p-2 text-gray-400 hover:text-gray-500"
-                            onClick={() => setOpenCart(false)}
+                            onClick={() => onCartToggle(false)}
                           >
                             <span className="sr-only">Close panel</span>
                             <XIcon className="h-6 w-6" aria-hidden="true" />
@@ -82,10 +55,8 @@ export default function ShoppingCart({ openCart, setOpenCart }) {
 
                       <div className="mt-8">
                         <div className="flow-root">
-                          <ul
-                            className="-my-6 divide-y divide-gray-200"
-                          >
-                            {products.map((product) => (
+                          <ul className="-my-6 divide-y divide-gray-200">
+                            {cartItems.map((product) => (
                               <li key={product.id} className="flex py-6">
                                 <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
                                   <img
@@ -104,7 +75,9 @@ export default function ShoppingCart({ openCart, setOpenCart }) {
                                           {product.name}{" "}
                                         </a>
                                       </h3>
-                                      <p className="ml-4">{product.price}</p>
+                                      <p className="ml-4">
+                                        Ksh. {product.price}.00
+                                      </p>
                                     </div>
                                     <p className="mt-1 text-sm text-gray-500">
                                       {product.color}
@@ -112,13 +85,19 @@ export default function ShoppingCart({ openCart, setOpenCart }) {
                                   </div>
                                   <div className="flex flex-1 items-end justify-between text-sm">
                                     <p className="text-gray-500">
-                                      Qty {product.quantity}
+                                      <span className="bg-yellow-300 p-2 m-2 rounded-full">
+                                        -
+                                      </span>{" "}
+                                      {product.quantity}
+                                      <span className="bg-yellow-300 p-2 m-2 rounded-full">
+                                        +
+                                      </span>
                                     </p>
 
                                     <div className="flex">
                                       <button
                                         type="button"
-                                        className="font-medium text-indigo-600 hover:text-indigo-500"
+                                        className="font-medium text-red-600 hover:text-red-500"
                                       >
                                         Remove
                                       </button>
@@ -135,7 +114,7 @@ export default function ShoppingCart({ openCart, setOpenCart }) {
                     <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
                       <div className="flex justify-between text-base font-medium text-gray-900">
                         <p>Subtotal</p>
-                        <p>$262.00</p>
+                        <p>Ksh. {totalPrice}.00</p>
                       </div>
                       <p className="mt-0.5 text-sm text-gray-500">
                         Shipping and taxes calculated at checkout.
@@ -143,7 +122,7 @@ export default function ShoppingCart({ openCart, setOpenCart }) {
                       <div className="mt-6">
                         <a
                           href="#"
-                          className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                          className="flex items-center justify-center rounded-md border border-transparent bg-red-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-red-700"
                         >
                           Checkout
                         </a>
@@ -153,8 +132,8 @@ export default function ShoppingCart({ openCart, setOpenCart }) {
                           or{" "}
                           <button
                             type="button"
-                            className="font-medium text-indigo-600 hover:text-indigo-500"
-                            onClick={() => setOpenCart(false)}
+                            className="font-medium text-red-600 hover:text-red-500"
+                            onClick={() => onCartToggle(false)}
                           >
                             Continue Shopping
                             <span aria-hidden="true"> &rarr;</span>
