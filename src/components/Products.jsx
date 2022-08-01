fa3b85c2e3960dde89dcd465bba57a3efd1ff237
kiborgok <borgokalek14@gmail.com>
2022-08-01 15:21:49 +0300

    show all products

---
+++ src/components/Products.jsx
@@ -0,0 +1,23 @@
+import Product from "./Product";
+
+export default function Products({ products, onProductHover }) {
+  return (
+    <div className="py-12 bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        {/* <div className="lg:text-center">
+          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+            Product
+          </h2>
+        </div> */}
+
+        <div className="mt-10">
+          <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-14">
+            {products.map((product) => (
+              <Product product={product} onProductHover={onProductHover} />
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
+  );
+}
